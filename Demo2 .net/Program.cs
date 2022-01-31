7bbf4648514bd0cb60a3c3a3a0dc882cdf3c5d7f
NICKN1 <iranildo.junior@outlook.com>
2022-01-31 15:50:53 -0300

    Utilização de 'ref' nos métodos

---
+++ Demo2 .net/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+
+namespace Demo2_.net
+{
+    //Aula Utilização do "ref"
+    class Program
+    {
+        /*A primera forma de utilização de ref é colocando ele na declaração
+        dos parâmetos do método e após na chamada do método
+        */
+
+        //======================================================
+
+        //Cria o método, referenciando a variável
+        static void Adcionar20(ref int a){
+            a += 20;
+        }
+        static void Main(string[] args)
+        {
+            //Cria variável
+            int a = 10;
+            //Chama o método, usando "ref" 
+            Adcionar20(ref a); 
+            WriteLine($"O valor de a é: {a}");
+        }
+    }
+}
